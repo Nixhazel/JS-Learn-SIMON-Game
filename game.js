5fd4c03ed26fe4ac6fd70aee9fc7c290486ab56f
Emmanuel Oghenenyore Olokor <38895255+Nixhazel@users.noreply.github.com>
2023-03-27 02:09:34 +0100

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
-$(document).on("tap", function () {
+$(document).on(function () {
 	if (!started) {
 		$("#level-title").text("Level " + level);
 		nextSequence();
